17c99ba8b5e3da0365c32c19bb4b50c4fc38b52f
nettiopsu <nettiopsu@users.noreply.github.com>
2020-02-25 19:15:08 +0200

    Icon update

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/dt.png` // This path is relative to the root of the site.
+        icon: `src/images/dt.svg` // This path is relative to the root of the site.
       }
     },
     {
