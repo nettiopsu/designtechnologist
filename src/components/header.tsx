237ac10057f7159f269d1f73256858de40cfa9fb
nettiopsu <nettiopsu@users.noreply.github.com>
2020-03-06 14:56:07 +0200

    Header design update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,33 +1,42 @@
 import { Link } from "gatsby";
 import React from "react";
 import Logo from "../images/logo.svg";
+import GithubLogo from "../icons/logo-github.svg";
+import SlackLogo from "../icons/logo-slack.svg";
+import HeaderStyles from "./header.module.css";
 
-interface Props {
-  siteTitle: string;
-}
-
-const Header = ({ siteTitle = "" }: Props) => (
-  <header
-    style={{
-      background: `#0899BC`
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        height: `64px`
-      }}
-    >
-      <Link
-        to="/"
-        style={{
-          color: `white`,
-          textDecoration: `none`
-        }}
-      >
-        <img src={Logo} style={{ display: "inline-block" }} />
-      </Link>
+const Header = () => (
+  <header className={HeaderStyles.root}>
+    <div className={HeaderStyles.container}>
+      <div className={HeaderStyles.left}>
+        <Link to="/">
+          <img src={Logo} className={HeaderStyles.logo} />
+        </Link>
+      </div>
+      <div className={HeaderStyles.right}>
+        <a
+          href="https://github.com/nettiopsu/designtechnologist"
+          target="_blank"
+          className={HeaderStyles.iconLink}
+        >
+          <img
+            src={GithubLogo}
+            alt="Github repository"
+            className={HeaderStyles.icon}
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://join.slack.com/t/designtechnologists/shared_invite/enQtOTU2MTc4OTE5NDI3LTUzYzZhM2Y0NDEwODgzZWNjNTAxYzVjM2EyNTI4ODIyYjFmZTg1YTRmOTdiNmQ0MDNkZGFhMTFiOGUxNDc5NmM"
+          className={HeaderStyles.iconLink}
+        >
+          <img
+            src={SlackLogo}
+            alt="Slack channel"
+            className={HeaderStyles.icon}
+          />
+        </a>
+      </div>
     </div>
   </header>
 );
