68ff9f9ac38b84cae2d88b2f5d52360368588046
nettiopsu <nettiopsu@users.noreply.github.com>
2020-09-06 19:54:59 +0300

    Putting header links into footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,17 @@ import FooterStyles from "./footer.module.css";
 const Footer = () => (
   <footer>
     <div className={FooterStyles.container}>
+      <a href="https://github.com/nettiopsu/designtechnologist" target="_blank">
+        GitHub
+      </a>
+      <a
+        target="_blank"
+        href="https://join.slack.com/t/designtechnologists/shared_invite/enQtOTU2MTc4OTE5NDI3LTUzYzZhM2Y0NDEwODgzZWNjNTAxYzVjM2EyNTI4ODIyYjFmZTg1YTRmOTdiNmQ0MDNkZGFhMTFiOGUxNDc5NmM"
+      >
+        Slack
+      </a>
       <a href="/license">License</a>
+      <a href="/contact">Contact us</a>
     </div>
   </footer>
 );
