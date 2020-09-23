afb80e617afbd09dbea5b2ca3fd4f62f9c510bf6
nettiopsu <nettiopsu@users.noreply.github.com>
2020-09-23 22:25:13 +0300

    Newsletter UI component and handler

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,27 +1,34 @@
 import React from "react";
 import FooterStyles from "./footer.module.css";
+import LayoutStyles from "./layout.module.css";
+import Newsletter from "./newsletter";
 
-const Footer = () => (
-  <footer>
-    <div className={FooterStyles.container}>
-      <a
-        target="_blank"
-        rel="noopener"
-        href="https://github.com/nettiopsu/designtechnologist"
-      >
-        GitHub
-      </a>
-      <a
-        target="_blank"
-        rel="noopener"
-        href="https://join.slack.com/t/designtechnologists/shared_invite/enQtOTU2MTc4OTE5NDI3LTUzYzZhM2Y0NDEwODgzZWNjNTAxYzVjM2EyNTI4ODIyYjFmZTg1YTRmOTdiNmQ0MDNkZGFhMTFiOGUxNDc5NmM"
-      >
-        Slack
-      </a>
-      <a href="/license">License</a>
-      <a href="/contact">Contact us</a>
-    </div>
-  </footer>
-);
+const Footer = () => {
+  return (
+    <footer>
+      <div className={LayoutStyles.content}>
+        <Newsletter />
+      </div>
+      <div className={FooterStyles.container}>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://github.com/nettiopsu/designtechnologist"
+        >
+          GitHub
+        </a>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://join.slack.com/t/designtechnologists/shared_invite/enQtOTU2MTc4OTE5NDI3LTUzYzZhM2Y0NDEwODgzZWNjNTAxYzVjM2EyNTI4ODIyYjFmZTg1YTRmOTdiNmQ0MDNkZGFhMTFiOGUxNDc5NmM"
+        >
+          Slack
+        </a>
+        <a href="/license">License</a>
+        <a href="/contact">Contact us</a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
---
+++ src/components/newsletter.tsx
@@ -0,0 +1,66 @@
+import React, { FormEvent, useRef, useState } from "react";
+import NewsletterStyles from "./newsletter.module.css";
+import addToMailchimp from "gatsby-plugin-mailchimp";
+
+const Newsletter = () => {
+  const email = useRef<HTMLInputElement | null>(null);
+  const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
+  const [message, setMessage] = useState<string | null>(null);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const currentEmail = email.current ? email.current.value : null;
+    if (currentEmail) {
+      const { result, msg } = await addToMailchimp(currentEmail, {
+        URL: window.location.pathname
+      });
+      if (result === "success") {
+        setIsSuccess(true);
+      } else {
+        setIsSuccess(false);
+      }
+      setMessage(msg);
+    }
+
+    return null;
+  };
+
+  return (
+    <div className={NewsletterStyles.container}>
+      <form onSubmit={e => handleSubmit(e)}>
+        <div className={NewsletterStyles.header}>
+          Design Technologist Club Newsletter
+        </div>
+        <div className={NewsletterStyles.text}>
+          Join Design Technologist Club and receive technology news, manuals,
+          tips and tricks
+        </div>
+        {!isSuccess && (
+          <div className={NewsletterStyles.text}>
+            <div className="flex items-center">
+              <input
+                type="type"
+                id="email-subscription"
+                ref={email}
+                placeholder={"E-mail"}
+                aria-label="E-mail"
+                className="w-full appearance-none border rounded w-full mr-3 py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+              />
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-6 rounded"
+              >
+                Subscribe
+              </button>
+            </div>
+          </div>
+        )}
+        <div id="woop" className={NewsletterStyles.text} role="alert">
+          {message}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Newsletter;
