da8c8e62595a70f6f80946b2f0d4603133ffb0e7
nettiopsu <nettiopsu@users.noreply.github.com>
2021-02-06 01:50:06 +0200

    Handbook page

---
+++ src/pages/book.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { graphql } from "gatsby";
+import mainPic from "../images/book.svg";
+import HandbookStyles from "./book.module.css";
+
+interface Edge {
+  node: {
+    frontmatter: {
+      title?: string;
+      order: number;
+    };
+    fields: {
+      slug: string;
+    };
+  };
+}
+
+interface Props {
+  data: {
+    allMdx: {
+      edges: Edge[];
+    };
+  };
+}
+
+const BookPage = ({
+  data: {
+    allMdx: { edges }
+  }
+}: Props) => {
+  return (
+    <Layout>
+      <SEO title="Design Technologist Handbook" />
+      <figure className={HandbookStyles.hero}>
+        <img src={mainPic} alt="" />
+      </figure>
+      <h1>Design Technologist Handbook</h1>
+      <ol>
+        {edges.map(edge => {
+          return (
+            <li key={"article-" + edge.node.fields.slug}>
+              <a href={"/" + edge.node.fields.slug}>
+                {edge.node.frontmatter.title}
+              </a>
+            </li>
+          );
+        })}
+      </ol>
+    </Layout>
+  );
+};
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMdx(sort: { order: ASC, fields: [frontmatter___order] }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            order
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default BookPage;
