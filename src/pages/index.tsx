4f07776eee803e7bfa99db8f65c6f72272cd12dc
nettiopsu <nettiopsu@users.noreply.github.com>
2021-02-06 02:10:19 +0200

    Index page update - intro text and two columns layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,13 @@ import SEO from "../components/seo";
 import { graphql } from "gatsby";
 import mainPic from "../images/designtechnologist.svg";
 import IndexStyles from "./index.module.css";
+import { Row, Col } from "react-flexbox-grid";
+import Signature from "../components/signature";
 
 interface Edge {
   node: {
     frontmatter: {
-      title: string;
+      title?: string;
       order: number;
     };
     fields: {
@@ -38,27 +40,70 @@ interface Props {
 
 const IndexPage = ({
   data: {
-    allMdx: { edges },
-    site
+    allMdx: { edges }
   }
 }: Props) => {
   return (
-    <Layout>
+    <Layout twoColumns={true}>
       <SEO />
-      <h1>{site.siteMetadata.title}</h1>
-      <figure className={IndexStyles.hero}>
-        <img src={mainPic} alt="" />
-      </figure>
-      <h2>Chapters</h2>
-      <ol>
-        {edges.map(edge => {
-          return (
-            <li key={"article-" + edge.node.fields.slug}>
-              <a href={edge.node.fields.slug}>{edge.node.frontmatter.title}</a>
-            </li>
-          );
-        })}
-      </ol>
+      <h1 className={IndexStyles.title}>Design Technologist Club</h1>
+      <Row>
+        <Col xs={12} lg={4}>
+          <div className={IndexStyles.left}>
+            <p>
+              <b>Not a pure developer, but neither a designer.</b>
+            </p>
+            <p>
+              <b>You exist in a gray zone.</b> You know how to develop, you have
+              an eye on UX and design, technology is just a tool for you to
+              satisfy user needs...
+            </p>
+            <p>
+              Sounds familiar? <b>Then you might be a Design Technologist.</b>
+            </p>
+            <p>
+              <b>I know how it feels</b>. Design Technologist, UX Developer,
+              Creative Technologist... <b>It has many names</b>, and{" "}
+              <b>I battled for many years</b> to understand who I am -
+              throughout my job in different companies and studying in
+              universities. <b>I could give up...</b>
+            </p>
+            <p>
+              <b>But I did not.</b> Instead, I researched and collected data
+              from various sources and <b>wrote a handbook</b> that you can read
+              here. I felt that I need to help others, who struggle with the
+              same questions: who they are, how their job title should be
+              called, and how to find your dream job. My aim is also to increase
+              the awareness of employers about{" "}
+              <b>how Design Technologists can be useful</b>.
+            </p>
+            <p>
+              And do not forget to <b>join our club</b> of Design Technologists
+              in Slack!
+            </p>
+            <Signature />
+          </div>
+        </Col>
+        <Col xs={12} lg={8}>
+          <div className={IndexStyles.right}>
+            <figure className={IndexStyles.hero}>
+              <img src={mainPic} alt="" />
+            </figure>
+            <h2>Handbook</h2>
+            <ol>
+              {edges.map(edge => {
+                return (
+                  <li key={"article-" + edge.node.fields.slug}>
+                    <a href={edge.node.fields.slug}>
+                      {edge.node.frontmatter.title}
+                    </a>
+                  </li>
+                );
+              })}
+            </ol>
+          </div>
+        </Col>
+      </Row>
     </Layout>
   );
 };
