c9474ad63c0a21b5d5f97fa8a8ea83995f5f2836
nettiopsu <nettiopsu@users.noreply.github.com>
2021-02-06 02:05:27 +0200

    Footer update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Container from "./container";
 import FooterStyles from "./footer.module.css";
 import LayoutStyles from "./layout.module.css";
 import Newsletter from "./newsletter";
@@ -6,21 +7,18 @@ import Newsletter from "./newsletter";
 const Footer = () => {
   return (
     <footer>
-      <div className={LayoutStyles.content}>
-        <Newsletter />
-      </div>
-      <div className={FooterStyles.container}>
-        <a
-          target="_blank"
-          rel="noopener"
-          href="https://github.com/nettiopsu/designtechnologist"
-        >
-          GitHub
-        </a>
-        <a href="/slack">Slack community</a>
-        <a href="/license">License</a>
-        <a href="/contact">Contact us</a>
-      </div>
+      <Container>
+        <div className={LayoutStyles.content}>
+          <Newsletter />
+        </div>
+        <div className={FooterStyles.container}>
+          <a href="/book">Handbook</a>
+          <a href="/slack">Slack community</a>
+          <a href="/newsletter">Newsletter</a>
+          <a href="/contact">Contacts</a>
+          <a href="/license">License</a>
+        </div>
+      </Container>
     </footer>
   );
 };
