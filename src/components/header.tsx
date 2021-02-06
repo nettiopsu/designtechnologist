c7b89e851f4c4179dbc92765a3bc32bfda8170e4
nettiopsu <nettiopsu@users.noreply.github.com>
2021-02-06 02:06:44 +0200

    Header update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,11 @@
-import { Link, useStaticQuery, graphql } from "gatsby";
+import { graphql, Link, useStaticQuery } from "gatsby";
 import React from "react";
-import Logo from "../images/logo.svg";
-import GithubLogo from "../icons/logo-github.svg";
+import BookLogo from "../icons/bookmark.svg";
 import SlackLogo from "../icons/logo-slack.svg";
 import MailIcon from "../icons/mail.svg";
+import NewsletterIcon from "../icons/newspaper.svg";
+import Logo from "../images/logo.svg";
+import Container from "./container";
 import HeaderStyles from "./header.module.css";
 
 const Header = () => {
@@ -22,51 +24,42 @@ const Header = () => {
 
   return (
     <header className={HeaderStyles.root}>
-      <div className={HeaderStyles.container}>
-        <div className={HeaderStyles.left}>
-          <Link to="/">
-            <img
-              src={Logo}
-              className={HeaderStyles.logo}
-              alt={site.siteMetadata.title}
-            />
-          </Link>
+      <Container twoColumns={true}>
+        <div className={HeaderStyles.container}>
+          <div className={HeaderStyles.left}>
+            <Link to="/">
+              <img
+                src={Logo}
+                className={HeaderStyles.logo}
+                alt={site.siteMetadata.title}
+              />
+            </Link>
+          </div>
+          <div className={HeaderStyles.right}>
+            <a className={HeaderStyles.iconLink} href="/book">
+              <img src={BookLogo} alt="" className={HeaderStyles.icon} />
+              Handbook
+            </a>
+            <a className={HeaderStyles.iconLink} href="/slack">
+              <img src={SlackLogo} alt="" className={HeaderStyles.icon} />
+              Slack community
+            </a>
+            <a className={HeaderStyles.iconLink} href="/newsletter">
+              <img src={NewsletterIcon} alt="" className={HeaderStyles.icon} />
+              Newsletter
+            </a>
+            <a className={HeaderStyles.iconLink} href="/contact">
+              <img
+                src={MailIcon}
+                alt=""
+                className={HeaderStyles.icon}
+                aria-hidden="true"
+              />
+              Contacts
+            </a>
+          </div>
         </div>
-        <div className={HeaderStyles.right}>
-          <a className={HeaderStyles.iconLink} href="/slack">
-            <img
-              src={SlackLogo}
-              alt="Slack community"
-              className={HeaderStyles.icon}
-              aria-hidden="true"
-            />
-            Slack community
-          </a>
-          <a
-            rel="noopener"
-            target="_blank"
-            className={HeaderStyles.iconLink}
-            href="https://github.com/nettiopsu/designtechnologist"
-          >
-            <img
-              src={GithubLogo}
-              alt="Github repository"
-              className={HeaderStyles.icon}
-              aria-hidden="true"
-            />
-            Github repository
-          </a>
-          <a className={HeaderStyles.iconLink} href="/contact">
-            <img
-              src={MailIcon}
-              alt="Contact us"
-              className={HeaderStyles.icon}
-              aria-hidden="true"
-            />
-            Contact us
-          </a>
-        </div>
-      </div>
+      </Container>
     </header>
   );
 };
