5d1d8c1fff1df4caaa890869b529882e66644114
nettiopsu <nettiopsu@users.noreply.github.com>
2021-02-13 13:28:28 +0200

    Hamburger menu with basic accessibility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 import { graphql, Link, useStaticQuery } from "gatsby";
-import React from "react";
+import React, { useState } from "react";
 import BookLogo from "../icons/bookmark.svg";
 import SlackLogo from "../icons/logo-slack.svg";
 import MailIcon from "../icons/mail.svg";
@@ -7,8 +7,12 @@ import NewsletterIcon from "../icons/newspaper.svg";
 import Logo from "../images/logo.svg";
 import Container from "./container";
 import HeaderStyles from "./header.module.css";
+import CloseIcon from "../icons/close.svg";
+import MenuIcon from "../icons/menu.svg";
 
 const Header = () => {
+  const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -35,7 +39,28 @@ const Header = () => {
               />
             </Link>
           </div>
-          <div className={HeaderStyles.right}>
+          <div className={HeaderStyles.mobileMenuContainer}>
+            <button
+              className={HeaderStyles.mobileMenuButton}
+              type="button"
+              onClick={() => {
+                setMobileMenuIsOpen(!mobileMenuIsOpen);
+              }}
+              aria-label="Menu"
+              aria-controls="menu"
+              aria-expanded={mobileMenuIsOpen ? "true" : undefined}
+              aria-haspopup="true"
+            >
+              <img
+                src={mobileMenuIsOpen ? CloseIcon : MenuIcon}
+                className={HeaderStyles.mobileMenuIcon}
+              />
+            </button>
+          </div>
+          <nav
+            className={HeaderStyles.right}
+            style={{ display: mobileMenuIsOpen ? "block" : undefined }}
+          >
             <a className={HeaderStyles.iconLink} href="/book">
               <img src={BookLogo} alt="" className={HeaderStyles.icon} />
               Handbook
@@ -57,7 +82,7 @@ const Header = () => {
               />
               Contacts
             </a>
-          </div>
+          </nav>
         </div>
       </Container>
     </header>
