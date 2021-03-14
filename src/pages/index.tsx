3f55d128949308972ffc1fa32bd454481122157a
nettiopsu <nettiopsu@users.noreply.github.com>
2021-03-14 20:12:45 +0200

    Main page text update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,44 +55,8 @@ const IndexPage = ({
       <SEO />
       <h1 className={indexTitle}>Design Technologist Club</h1>
       <Row>
-        <Col xs={12} lg={4}>
-          <div className={indexLeft}>
-            <p>
-              <b>Not a pure developer, but neither a designer.</b>
-            </p>
-            <p>
-              <b>You exist in a gray zone.</b> You know how to develop, you have
-              an eye on UX and design, technology is just a tool for you to
-              satisfy user needs...
-            </p>
-            <p>
-              Sounds familiar? <b>Then you might be a Design Technologist.</b>
-            </p>
-            <p>
-              <b>I know how it feels</b>. Design Technologist, UX Developer,
-              Creative Technologist... <b>It has many names</b>, and{" "}
-              <b>I battled for many years</b> to understand who I am -
-              throughout my job in different companies and studying in
-              universities. <b>I could give up...</b>
-            </p>
-            <p>
-              <b>But I did not.</b> Instead, I researched and collected data
-              from various sources and <b>wrote a handbook</b> that you can read
-              here. I felt that I need to help others, who struggle with the
-              same questions: who they are, how their job title should be
-              called, and how to find your dream job. My aim is also to increase
-              the awareness of employers about{" "}
-              <b>how Design Technologists can be useful</b>.
-            </p>
-            <p>
-              And do not forget to <b>join our club</b> of Design Technologists
-              in Slack!
-            </p>
-            <Signature />
-          </div>
-        </Col>
         <Col xs={12} lg={8}>
-          <div className={indexRight}>
+          <div className={indexLeft}>
             <figure className={indexHero}>
               <img src={mainPic} alt="" />
             </figure>
@@ -109,6 +73,37 @@ const IndexPage = ({
                 );
               })}
             </ol>
+          </div>
+        </Col>
+        <Col xs={12} lg={4}>
+          <div className={indexRight}>
+            <p>
+              <b>Not a pure developer, but neither a designer.</b>
+            </p>
+            <p>
+              <b>You exist in a gray zone.</b> You know how to develop, you have
+              an eye for UX and design, technology is just a tool for you to
+              satisfy user needs...
+            </p>
+            <p>
+              Sounds familiar? <b>You might be a Design Technologist.</b>
+            </p>
+            <p>Join our club of like-minded people and check the handbook:</p>
+            <ul>
+              <li>
+                <a href="/book">Design Technologist Handbook</a>
+              </li>
+              <li>
+                <a href="/slack">Join the Slack community</a>
+              </li>
+              <li>
+                <a href="/newsletter">Subscribe to the newsletter</a>
+              </li>
+              <li>
+                <a href="/about">About the author</a>
+              </li>
+            </ul>
+            <Signature />
           </div>
         </Col>
       </Row>
