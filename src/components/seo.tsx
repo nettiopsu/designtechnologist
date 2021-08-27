ef7df239b245474703b5b6f0a0652f9803d51b36
nettiopsu <nettiopsu@users.noreply.github.com>
2021-08-27 21:31:24 +0300

    Title emoji update

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -35,7 +35,7 @@ function SEO(props: SiteProps) {
       }}
       title={
         title
-          ? title + " 💯 " + (mainTitle || site.siteMetadata.title)
+          ? title + " 🚀 " + (mainTitle || site.siteMetadata.title)
           : site.siteMetadata.title
       }
       meta={[
