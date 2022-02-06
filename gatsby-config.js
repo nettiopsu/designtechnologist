3dc4076ac5c815a71de4c9ca90973e6b1079465a
nettiopsu <nettiopsu@users.noreply.github.com>
2022-02-06 17:10:03 +0200

    Removed unused variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,8 +114,7 @@ module.exports = {
   flags: {
     FAST_DEV: true,
     PRESERVE_FILE_DOWNLOAD_CACHE: true,
-    PARALLEL_SOURCING: true,
-    LMDB_STORE: true
+    PARALLEL_SOURCING: true
   },
   plugins
 };
